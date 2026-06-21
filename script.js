6b110d0c2a8c642d0fd6b045b7392f006bfb2199
HunterTigerX <HunterTigerX@gmail.com>
2026-06-21 12:23:32 +0300

    feat: add favicon 21.06.2026 12:23:31

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// let test2 = document.querySelector('span');
-// let nav_bar = document.querySelectorAll('.tttt')
-
-// parent.onmouseover = parent.onmouseout = parent.onmousemove = handler;
-
-// function handler(event) {
-//     if (event.target.classList.contains('test')) {
-//         test2.classList.remove('test');
-//     }
-//     if (event.target.classList.contains('tttt')) {
-//         test2.classList.remove('test');
-//     }
-// }
-
 console.log("Self-assessment for main page:\n" +
 "✅ Markup validation (10 points):\n" +
 "  - Valid HTML, no errors/warnings from validator.w3.org (+5)\n" +
