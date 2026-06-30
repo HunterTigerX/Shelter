502494f93ddad954e12e6a6e4f65824151617223
HunterTigerX <HunterTigerX@gmail.com>
2026-06-30 19:07:26 +0300

    fix: fix paths 30.06.2026 19:07:26

--- pets/script_pets.js
+++ pets/script_pets.js
@@ -1,4 +1,4 @@
 let header__block_logo = document.querySelector(".header__block-logo");
 header__block_logo.addEventListener("click", function () {
-  window.open("../main/index.html", (target = "_self"));
+  window.open("../index.html", (target = "_self"));
 });
